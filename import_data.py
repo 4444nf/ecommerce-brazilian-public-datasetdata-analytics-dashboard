c9f206bf06031945ea3d83f5773c6e0781518c13
4444nf <fanninegyesi@gmail.com>
2026-05-11 21:34:44 +0200

    feat: adding python script which imports data from csv to postgresql db

---
+++ import_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+
+file_list = ["olist_customers_dataset.csv",
+             "olist_geolocation_dataset.csv",
+             "olist_order_items_dataset.csv",
+             "olist_order_payments_dataset.csv",
+             "olist_order_reviews_dataset.csv",
+             "olist_orders_dataset.csv",
+             "olist_products_dataset.csv",
+             "olist_sellers_dataset.csv",
+             "product_category_name_translation.csv"]
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
+
+for file in file_list:
+    df = pd.read_csv(file)
+    table_name = file.replace(".csv", "")
+    df.to_sql(table_name, engine, if_exists="replace", index= False)
+    print(f"{table_name} imported successfully")
